3f210efc389df2392f2b20397032605e446c9366
blburkholder <blburkholder@alaska.edu>
2016-10-28 15:26:10 -0800

    pretty plots for paper

---
+++ crossings_of_interest_2.m
@@ -0,0 +1,29 @@
+function [crossings] = crossings_of_interest_2(boundaries)
+
+where_the_SWTS_at = strfind(boundaries(7,:),[-1, 2])+1;
+where_the_STSW_at = strfind(boundaries(7,:),[2,-1])+1;
+
+SW_to_sheath = boundaries(:,where_the_SWTS_at);
+sheath_to_SW = boundaries(:,where_the_STSW_at);
+
+SW_to_sheath(8,:) = 24*60*(datenum(SW_to_sheath(1,:),SW_to_sheath(2,:),SW_to_sheath(3,:),...
+    SW_to_sheath(4,:),SW_to_sheath(5,:),SW_to_sheath(6,:)) - datenum(2004,1,1));
+
+sheath_to_SW(8,:) = 24*60*(datenum(sheath_to_SW(1,:),sheath_to_SW(2,:),sheath_to_SW(3,:),...
+    sheath_to_SW(4,:),sheath_to_SW(5,:),sheath_to_SW(6,:)) - datenum(2004,1,1));
+
+SWTS_sheath_begin = 24*60*(datenum(boundaries(1,where_the_SWTS_at-1),boundaries(2,where_the_SWTS_at-1),...
+    boundaries(3,where_the_SWTS_at-1),boundaries(4,where_the_SWTS_at-1),...
+    boundaries(5,where_the_SWTS_at-1),boundaries(6,where_the_SWTS_at-1)) - datenum(2004,1,1));
+
+STSW_next_begin = 24*60*(datenum(boundaries(1,where_the_STSW_at+1),boundaries(2,where_the_STSW_at+1),...
+    boundaries(3,where_the_STSW_at+1),boundaries(4,where_the_STSW_at+1),...
+    boundaries(5,where_the_STSW_at+1),boundaries(6,where_the_STSW_at+1)) - datenum(2004,1,1));
+
+SW_to_sheath(9,:) = SW_to_sheath(8,:) - SWTS_sheath_begin;
+sheath_to_SW(9,:) = STSW_next_begin - sheath_to_SW(8,:);
+
+c = horzcat(SW_to_sheath,sheath_to_SW);
+crossings = transpose(sortrows(transpose(c),8));
+
+end
